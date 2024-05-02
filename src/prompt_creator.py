2f16e0012ee8fb400bb301572033a9a2874457f7
harisk <hk8302@student.uni-lj.si>
2024-05-02 10:51:29 +0200

    added metrics - classification accuracy

--- src/prompt_creator.py
+++ src/prompt_creator.py
@@ -1,24 +1,26 @@
-import os
 import json
+import os
 import random
-from utils_funs import load_data
+
+from utils_funs import calculate_accuracy, load_data
+
 
 class PromptCreator:
-    
+
     INSTRUCTION_START = "[INST]"
     INSTRUCTION_END = "[/INST]"
-    
+
     def __init__(self, strategy, data):
         self.strategy = strategy
         self.current_index = 0
         self.q, self.a, self.ids = data
         self.length = len(self.q)
-                      
-    def get_next_prompt(self, extra_instructions = None, add_beginning_of_answer = False):
+
+    def get_next_prompt(self, extra_instructions=None, add_beginning_of_answer=False):
         question = self.q[self.current_index]
         self.current_index += 1
         return self.create_prompt(question, extra_instructions)
-    
+
     def zero_shot_CoT(self):
         return "A: Let's think step by step."
 
@@ -27,36 +29,37 @@ class PromptCreator:
 
     def no_prompting():
         return "A:"
-    
+
     def wrap_in_instructions(self, text):
         return self.INSTRUCTION_START + "\n" + text + self.INSTRUCTION_END + "\n"
-        
+
     def create_prompt(self, question, extra_instructions):
         stem_and_choices = self.wrap_in_instructions(question)
-        
+
         prompt = ""
-            
+
         if self.strategy == "zero_shot":
             start_of_answer = self.zero_shot_CoT()
-            prompt = stem_and_choices + start_of_answer            
+            prompt = stem_and_choices + start_of_answer
         elif self.strategy == "plan_and_solve":
             start_of_answer = self.plan_and_solve()
             prompt = stem_and_choices + start_of_answer
         elif self.strategy == "no_prompting":
             start_of_answer = self.no_prompting()
             prompt = stem_and_choices + start_of_answer
-            
+
         if extra_instructions:
             prompt = self.wrap_in_instructions(extra_instructions) + prompt
 
         return prompt
-    
+
+
 if __name__ == "__main__":
     # define dataset
-    dataset_name = "commonsenseqa" # "strategyqa"
+    dataset_name = "commonsenseqa"  # "strategyqa"
 
     # define strategy
-    strategy = "zero_shot" # "plan_and_solve", "zero_shot"
+    strategy = "zero_shot"  # "plan_and_solve", "zero_shot"
 
     # load data
     q, a, ids = load_data(dataset_name)
@@ -64,8 +67,13 @@ if __name__ == "__main__":
     # create prompt
     prompt_creator = PromptCreator(strategy, (q, a, ids))
 
-    res = []
-    for i in range(3): # range(len(q)):
-        print(f"Processing question {i+1}/{len(q)}")
-        prompt = prompt_creator.get_next_prompt(add_beginning_of_answer=True)
-        print(prompt)
+    evaluate = True
+
+    # res = []
+    # for i in range(3):  # range(len(q)):
+    #     print(f"Processing question {i+1}/{len(q)}")
+    #     prompt = prompt_creator.get_next_prompt(add_beginning_of_answer=True)
+    #     print(prompt)
+
+    if evaluate:
+        calculate_accuracy()
--- src/utils_funs.py
+++ src/utils_funs.py
@@ -1,37 +1,63 @@
-import re
 import json
+import re
+from pathlib import Path
+
+import numpy as np
+
+ROOT_DIR = str(Path(__file__).resolve().parents[1])
+
+
+def calculate_accuracy():
+    dataset = "commonsenseqa.json"
+    strategies = ["plan_and_solve", "zero_shot"]
+    results_path = ROOT_DIR + "/generated_result"
+
+    results = {}
+    for strategy in strategies:
+        with open(results_path + f"/{strategy}/{dataset}", "r") as f:
+            result = json.load(f)
+
+            acc = np.mean([result[i]["ans"] for i in range(len(result))])
+
+        results[strategy] = acc
+
+    print(results)
+
 
 def extract_answer(dataset, text):
     if dataset == "commonsenseqa":
         pred = text.strip()
         pred = re.sub("\(|\)|\:|\.|\,", "", pred)
         pred = pred.split()
-        pred_answer = [i for i in pred if i in ('A|B|C|D|E')][-1]
+        pred_answer = [i for i in pred if i in ("A|B|C|D|E")][-1]
         # pred_answer = re.findall(r'A|B|C|D|E', pred)[0]
         return pred_answer
     elif dataset == "strategyqa":
         pred = text.lower()
-        pred = re.sub("\"|\'|\n|\.|\s|\:|\,", " ", pred)
+        pred = re.sub("\"|'|\n|\.|\s|\:|\,", " ", pred)
         pred = pred.split()
         pred_answer = [i for i in pred if i in ("yes", "no")][-1]
         return pred_answer
     else:
-        raise NotImplementedError(' not support dataset: {}'.format(dataset))
-    
+        raise NotImplementedError(" not support dataset: {}".format(dataset))
+
+
 def load_data(dataset):
     decoder = json.JSONDecoder()
     questions = []
     answers = []
     ids = []
+
+    curr_dir = str(Path(__file__).resolve().parents[1])
     if dataset == "commonsenseqa":
-        datapath = "../datasets/CommonsenseQA/CommonsenseQA.jsonl"
+        datapath = curr_dir + "/datasets/CommonsenseQA/CommonsenseQA.jsonl"
     elif dataset == "strategyqa":
-        datapath = "../datasets/StrategyQA/StrategyQA.json"
+        datapath = curr_dir + "/datasets/StrategyQA/StrategyQA.json"
 
     # read dataset file
-    if dataset.lower() in ['strategyqa']:
+    if dataset.lower() in ["strategyqa"]:
         with open(datapath) as f:
-            if dataset.lower() in ['strategyqa']:
+            if dataset.lower() in ["strategyqa"]:
                 json_data = json.load(f)["examples"]
             else:
                 json_data = json.load(f)
@@ -43,11 +69,11 @@ def load_data(dataset):
                     a = "yes"
                 else:
                     a = "no"
-                id = 'temp_{}'.format(idx)
+                id = "temp_{}".format(idx)
                 questions.append(q)
                 answers.append(a)
                 ids.append(id)
-    elif dataset.lower() in ['commonsenseqa']:
+    elif dataset.lower() in ["commonsenseqa"]:
         with open(datapath) as f:
             lines = f.readlines()
             for idx, line in enumerate(lines):
@@ -60,16 +86,18 @@ def load_data(dataset):
                     choice += c["text"]
                 q = json_res["question"]["stem"].strip() + " " + choice
                 a = json_res["answerKey"]
-                id = 'temp_{}'.format(idx)
+                id = "temp_{}".format(idx)
                 questions.append(q)
                 answers.append(a)
                 ids.append(id)
 
     return questions, answers, ids
 
+
 def construct_input(prompt, text):
-    inputs = 'Q:' + text + "\nA: " + prompt
+    inputs = "Q:" + text + "\nA: " + prompt
     return inputs
+
 
 if __name__ == "__main__":
     text = "\n\nRelevant information: \nMona Lisa: Located in the Louvre Museum in Paris, France\nVenus de Milo: Located in the Louvre Museum in Paris, France\n\nPlan: \nStep 1: (A) Identify the locations of the Mona Lisa and the Venus de Milo\nStep 2: Compare the locations\n\nAnswer: \nYes, the Mona Lisa and the Venus de Milo are both located in the Louvre Museum in Paris, France."
