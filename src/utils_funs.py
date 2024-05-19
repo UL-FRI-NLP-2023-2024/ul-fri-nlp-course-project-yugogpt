7ba349eb68cab474f2dcd51a354d5942e2506650
Jovana Videnović <jv8043@student.uni-lj.si>
2024-05-19 12:06:01 +0200

    fixed evaluation, new results for self argument

--- src/utils_funs.py
+++ src/utils_funs.py
@@ -1,74 +1,32 @@
-import json
 import re
-from pathlib import Path
-
-import matplotlib.pyplot as plt
+import json
 import numpy as np
-
-plt.rc("text", usetex=True)
-plt.rc("font", family="serif")
-plt.style.use("ggplot")
-plt.rcParams["axes.facecolor"] = "w"
-plt.rcParams["grid.linestyle"] = "dashed"
-plt.rcParams["grid.linewidth"] = 0.5
-
-ROOT_DIR = str(Path(__file__).resolve().parents[1])
-
-
-def calculate_accuracy():
-    dataset = "commonsenseqa.json"
-    strategies = ["no_prompting", "zero_shot", "cot", "plan_and_solve"]
-    results_path = ROOT_DIR + "/generated_result"
-
-    _map_names = {
-        "no_prompting": "Raw Prompt",
-        "zero_shot": "Zero-Shot CoT",
-        "cot": "Chain-of-Thought",
-        "plan_and_solve": "Plan-and-Solve",
-    }
-    results = {}
-    for strategy in strategies:
-        with open(results_path + f"/{strategy}/{dataset}", "r") as f:
-            result = json.load(f)
-
-            acc = np.mean([result[i]["ans"] for i in range(len(result))])
-
-        results[_map_names[strategy]] = acc
-
-    fig, ax = plt.subplots()
-
-    bars = ax.bar(results.keys(), results.values(), width=0.4, color="indianred")
-    ax.bar_label(bars)
-    plt.ylabel("Accuracy")
-
-    ax.spines["bottom"].set_color("black")
-    ax.spines["left"].set_color("black")
-    plt.tight_layout()
-
-    plt.savefig(ROOT_DIR + "/generated_result/accuracy.pdf")
-    plt.show()
-
-    print(results)
-
 
 def extract_answer(dataset, text):
     if dataset == "commonsenseqa":
         pred = text.strip()
-        pred = re.sub("\(|\)|\:|\.|\,", "", pred)
+        pred = pred.replace(". A", "")
+        pred = pred.replace(": A", "")
+        pred = pred.replace("! A", "")
+        
+        pred = re.sub(r"\(|\)|\:|\.|\,", "", pred)
         pred = pred.split()
-        pred_answer = [i for i in pred if i in ("A|B|C|D|E")][-1]
-        # pred_answer = re.findall(r'A|B|C|D|E', pred)[0]
+        
+        pred_answer = [i for i in pred if i in ('A|B|C|D|E')]
+        if len(pred_answer) > 0:
+            pred_answer = pred_answer[-1]
+        else:
+            pred_answer = "X"
         return pred_answer
     elif dataset == "strategyqa":
         pred = text.lower()
-        pred = re.sub("\"|'|\n|\.|\s|\:|\,", " ", pred)
+        pred = re.sub("\"|\'|\n|\.|\s|\:|\,", " ", pred)
         pred = pred.split()
         pred_answer = [i for i in pred if i in ("yes", "no")][-1]
         return pred_answer
     else:
-        raise NotImplementedError(" not support dataset: {}".format(dataset))
-
-
+        raise NotImplementedError(' not support dataset: {}'.format(dataset))
+    
 def load_data(dataset):
     decoder = json.JSONDecoder()
     questions = []
@@ -83,9 +41,9 @@ def load_data(dataset):
         datapath = "../datasets/CommonsenseQA/CoT.json"
 
     # read dataset file
-    if dataset.lower() in ["strategyqa"]:
+    if dataset.lower() in ['strategyqa']:
         with open(datapath) as f:
-            if dataset.lower() in ["strategyqa"]:
+            if dataset.lower() in ['strategyqa']:
                 json_data = json.load(f)["examples"]
             else:
                 json_data = json.load(f)
@@ -97,11 +55,11 @@ def load_data(dataset):
                     a = "yes"
                 else:
                     a = "no"
-                id = "temp_{}".format(idx)
+                id = 'temp_{}'.format(idx)
                 questions.append(q)
                 answers.append(a)
                 ids.append(id)
-    elif dataset.lower() in ["commonsenseqa", "commonsenseqa_cot"]:
+    elif dataset.lower() in ['commonsenseqa', 'commonsenseqa_cot']:
         with open(datapath) as f:
             lines = f.readlines()
             for idx, line in enumerate(lines):
@@ -115,7 +73,7 @@ def load_data(dataset):
                 q = json_res["question"]["stem"].strip() + " " + choice
                 a = json_res["answerKey"]
 
-                id = "temp_{}".format(idx)
+                id = 'temp_{}'.format(idx)
                 questions.append(q)
                 answers.append(a)
                 ids.append(id)
@@ -128,13 +86,33 @@ def load_data(dataset):
         return questions, answers, ids, whole_answers
     return questions, answers, ids
 
-
 def construct_input(prompt, text):
-    inputs = "Q:" + text + "\nA: " + prompt
+    inputs = 'Q:' + text + "\nA: " + prompt
     return inputs
 
+def fix_previous_results(json_res_path, new_json_res_path):
+    with open(json_res_path) as f:
+        json_res = json.load(f)
+
+    new_json_res = json_res.copy()
+    correct_answers = 0
+    for res in json_res:
+        ans = extract_answer("commonsenseqa", res["whole_pred"])
+        if ans == res["answer"]:
+            correct_answers += 1
+        res["pred"] = ans
+        res["ans"] = res["answer"] == res["pred"]
+
+    with open(new_json_res_path, 'w') as f:
+        json.dump(new_json_res, f, indent=4)
+    
+    print("Correct answers: ", correct_answers)
+    print("Total answers: ", len(json_res))
+    print("Solve rate: ", correct_answers / len(json_res))
 
 if __name__ == "__main__":
-    text = "\n\nRelevant information: \nMona Lisa: Located in the Louvre Museum in Paris, France\nVenus de Milo: Located in the Louvre Museum in Paris, France\n\nPlan: \nStep 1: (A) Identify the locations of the Mona Lisa and the Venus de Milo\nStep 2: Compare the locations\n\nAnswer: \nYes, the Mona Lisa and the Venus de Milo are both located in the Louvre Museum in Paris, France."
-    ans = extract_answer("commonsenseqa", text)
-    print(ans)
+    # text = "Therefore, the most convincing answer is (B) chess game. A pawn is a versatile piece in chess because of its unique movements and abilities, which make it an important part of the game."
+    # ans = extract_answer("commonsenseqa", text)
+    # print(ans)
+
+    fix_previous_results("../generated_result/self_argument/self_argument.json", "../generated_result/self_argument/self_argument_new.json")
